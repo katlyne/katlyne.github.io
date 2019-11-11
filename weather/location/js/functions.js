e105c362764dcb4812812538624009668c15205b
katlyne <chi17400@byui.edu>
2019-11-11 11:47:55 -0700

    fetch

--- weather/location/js/functions.js
+++ weather/location/js/functions.js
@@ -50,6 +50,128 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
+/* ****************************************
+*   Fetch data
+***************************************** */
+function fetchWeatherData(weatherURL) {
+    let cityName = 'Preston'; // The data we want from the weather .json file
+    fetch(weatherURL)
+    .then(function(response) {
+        if(response.ok) {
+            return response .json();
+        }
+        throw new ERROR('Network response was not OK.');
+    })
+    .then(function(data){
+        //Check the data objet that was retrieved
+        console.log(data);
+        // data is the full JavaScript object, but we only want the preston part shorten the variable and focus only on the data we want to reduce typing
+        let p = data[cityName];
+
+        // ***********  Get the location information    **************
+        let locName = p.properties.relativeLocation.properties.city;
+        console.log(locName);
+        let locState = p.properties.relativeLocation.properties.state;
+        // Put them togather
+        let fullName = locName+', '+ locState;
+        // See if it worked, using ticks around the content in the log
+        console.log(`fullName is: ${fullName}`);
+        // Get the longitude and latitude and combine them to a comma separated single string
+        const latLong = p.properties.relativeLocation.geometry.coordinate[1] + ","+p.properties.relativeLocation.geometry.coordinates[0];
+        console.log(latLong);
+        // Create a JSON object containing the full name, latitude and longitude and store it into local storage.
+        sessStore.setItem("fullName", fullName);
+        sessStore.setItem("latLong", latLong);
+        // ********** Get the current conditions information  ***********
+        // As the data is extracted from the JSON, store it into session storage
+        const prestonData = JSON.stringify({fullName, latLong});
+        locStore.setItem("Preston, ID", prestonData);
+        // Get the temperature data
+        const prestontemp = p.properties.relativeLocation.properties.temperature;
+        console.log(prestontemp);
+        const phightemp = p.properties.relativeLocation.properties.highTemp;
+        console.log(phightemp);
+        sessStore.setItem("phightemp");
+        const plowtemp = p.properties.relativeLocation.properties.lowTemp;
+        console.log(plowtemp);
+        sessStore.setItem("plowtemp");
+        //const prestontemp = JSON.stringify({temperature});
+        sessStore.setItem("prestontemp", prestontemp)
+        // Get the wind data
+        const pwindspeed = p.properties.relativeLocation.properties.windSpeed;
+        console.log(pwindspeed);
+        sessStore.setItem("pwindspeed");
+        const pwindgust = p.properties.relativeLocation.properties.pwindgust;
+        console.log(pwindgust);
+        sessStore.setItem("pwindgust");
+
+        
+
+        // Get the hourly data using another function - should include the forecast temp, condition icons and wind speeds. The data will be stored into seesion storage.
+        getHourly(p.properties.forecastHourly);
+    })
+    .catch(function(error){
+        console.log('There was a fetch problem: ', error.message);
+        statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+    })
+}
+
+/* ****************************************
+*   Web Storage API
+**************************************   */    
+
+//Setup localStorage
+//var locStore = window.localStorage;
+
+// Setup sessionStorage
+//var sessStore = window.sessionStorage;
+
+// Store favorite class into localStorage
+//locStore.setItem('favoriteClass', 'CIT230');
+
+
+/* *************************************
+*  Get Hourly Forecast data
+************************************* */
+function getHourly(URL) {
+    fetch(URL)
+     .then(function (response) {
+      if (response.ok) {
+       return response.json();
+      }
+      throw new ERROR('Response not OK.');
+     })
+     .then(function (data) {
+      console.log('Data from getHourly function:');
+      console.log(data); // Let's see what we got back
+   
+      // Store 12 hours of data to session storage  
+      var hourData = [];
+      let todayDate = new Date();
+      var nowHour = todayDate.getHours();
+      console.log(`nowHour is ${nowHour}`);
+      for (let i = 0, x = 11; i <= x; i++) {
+       if (nowHour < 24) {
+        hourData[nowHour] = data.properties.periods[i].temperature + "," + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+        sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+        nowHour++;
+       } else {
+        nowHour = nowHour - 12;
+        hourData[nowHour] = data.properties.periods[i].temperature + "," + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+        sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+        nowHour = 1;
+       }
+      }
+   
+      // Get the shortForecast value from the first hour (the current hour)
+      // This will be the condition keyword for setting the background image
+      sessStore.setItem('shortForecast', data.properties.periods[0].shortForecast);
+   
+      // Call the buildPage function
+      buildPage();
+     })
+     .catch(error => console.log('There was a getHourly error: ', error))
+   }
 
 //date
 function lastModified() {
@@ -149,54 +271,5 @@ function changeSummaryImage(weather){
         break;
     }
 
-}
-
-/* ****************************************
-*   Fetch data
-***************************************** */
-function fetchWeatherData(weatherURL) {
-    let cityName = 'Preston'; // The data we want from the weather .json file
-    fetch(weatherURL)
-    .then(function(response) {
-        if(response.ok) {
-            return response .json();
-        }
-        throw new ERROR('Network response was not OK.');
-    })
-    .then(function(data){
-        //Check the data objet that was retrieved
-        console.log(data);
-        // data is the full JavaScript object, but we only want the preston part
-        //shorten the variable and focus only on the data we want to reduce typing
-        let p = data[cityName];
-
-        // ***********  Get the location information    **************
-        let cityName = p.City
-        let currState = p.State;
-        // Put them togather
-        let fullName = cityName+', '+currState;
-        // See if it worked, using ticks around the content in the log
-        console.log('fullName is: ${fullName}');
-        // Get the longitude and latitude and combine them to a comma separated single string
-        const latLong = p.properties.relativeLocation.geometry.coordinate[1] + ","+p.properties.relativeLocation.geometry.coordinates[0];
-        console.log(latLong);
-        // Create a JSON object containing the full name, latitude and longitude and store it into local storage.
-
-        // ********** Get the current conditions information    ***********
-        // As the data is extracted from the JSON, store it into session storage
-        const prestonDatoa = JSON.stringify({fullName, latLong});
-        locStore.setItem("Preston, ID", prestonData);
-        // Get the temperature data
-
-
-        // Get the wind data
-
-
-        // Get the hourly data using another function - should include the forecast temp, condition icons and wind speeds. The data will be stored into seesion storage.
-    })
-    .catch(function(error){
-        console.log('There was a fetch problem: ', error.message);
-        statusContainer.innerHTML = 'Sorry, the data could not be processed.';
-    })
 }
 
