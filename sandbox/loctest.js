e9d377e5940e7c368e529f0b70d91fcd5e000691
katlyne <chi17400@byui.edu>
2019-11-22 12:01:00 -0700

    loc

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// Set global variable for custom header required by NWS API
+var idHeader = {
+    headers: {
+     "User-Agent": "Student Learning Project - yourschoolemailaddress@byui.edu"
+    }
+   };
+
 // Call the function to get our location
 getGeoLocation();
 
@@ -26,9 +33,65 @@ function getGeoLocation() {
          const locale = lat + "," + long;
          console.log(`Lat and Long are: ${locale}.`);
          storage.setItem("locale", locale);
+         // Call getLocation function, send locale as a parameter
+        getLocation(locale);
       
         })
-       } else {
+       } 
+       else {
         status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
        } // end else
-} //end getGeoLocation
+       if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+         const lat = position.coords.latitude;
+         const long = position.coords.longitude;
+      
+         // Combine the values for use later
+         const locale = lat + "," + long;
+         console.log(`Locale values are: ${locale}.`);
+         // Store the values to session storage
+         window.sessionStorage.setItem("locale", locale);
+         window.sessionStorage.setItem("latitude", lat);
+         window.sessionStorage.setItem("longitude", long);
+      
+        })
+       } 
+       else {
+        status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+       } // end else
+      } //end getGeoLocation
+
+      // Gets location information from the NWS API
+function getLocation(locale) {
+ const URL = "https://api.weather.gov/points/" + locale; 
+ // NWS User-Agent header (built above) is the second parameter 
+ fetch(URL, idHeader) 
+ .then(function(response){
+   if(response.ok){ 
+    return response.json(); 
+   } 
+   throw new ERROR('Response not OK.');
+ })
+ .then(function (data) { 
+   // Let's see what we got back
+   console.log('Json object from getLocation function:'); 
+   console.log(data);
+   // Store data to sessionStorage 
+   window.sessionStorage.setItem("locName", data.properties.relativeLocation.properties.city); 
+   window.sessionStorage.setItem("locState", data.properties.relativeLocation.properties.state);
+   let fullName = data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state;
+   window.sessionStorage.setItem("fullName", fullName); 
+   
+   // Store three URL's for stationId's, forecast and hourly forecast
+   // The URL's are in the returned location data object
+   window.sessionStorage.setItem("hourlyURL", data.properties.forecastHourly);
+   window.sessionStorage.setItem("forecastURL", data.properties.forecast);
+   let stationsURL = data.properties.observationStations;
+   window.sessionStorage.setItem("stationsURL", stationsURL); 
+
+   // Call the function to get the list of weather stations
+   // using the URL for the weather station list
+   getStationId(stationsURL); 
+  }) 
+ .catch(error => console.log('There was a getLocation error: ', error)) 
+} // end getLocation function
